bd1e59cdb7ed083a1de9c5e29343cd292846de0e
张雷 <1023086065@qq.com>
2021-11-16 15:29:08 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'interferon',
       script: 'bin/www',
       watch: true,
-      ignore_watch: ['.git', 'node_modules', 'public', 'views','logs'],
+      ignore_watch: ['.git', 'node_modules', 'public', 'views', 'logs'],
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       error_file: 'logs/err.log',
       out_file: 'logs/out.log',
