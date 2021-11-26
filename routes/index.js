945bd305c3076b482e406d3d223a3aa8129058c9
Blaston <1023086065@qq.com>
2021-11-26 11:08:35 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/tree', function (req, res, next) {
-	res.render('tree');
+  res.render('tree');
 });
 
 router.get('/treeBird', function (req, res, next) {
-	res.render('treeBird');
+  res.render('treeBird');
 });
 
 router.get('/difference', function (req, res, next) {
-	res.render('difference');
+  res.render('difference');
 });
 
 router.get('/chromosome', function (req, res, next) {
-	res.render('chromosome');
+  res.render('chromosome');
+});
+
+router.get('/update', (req, res, next) => {
+  let exec = require('child_process').exec;
+  exec('git pull');
+  res.status(200).send('updated');
 });
 
 module.exports = router;
